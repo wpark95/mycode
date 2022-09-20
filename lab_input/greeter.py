7cae7b235cfc59e2b8872339a95bcd5291c565be
wpark95 <park.wongyun@gmail.com>
2022-09-20 15:34:31 +0000

    Add day 1 challenge (greeter.py)

--- lab_input/greeter.py
+++ lab_input/greeter.py
@@ -1,18 +1,30 @@
-# shebang
 #!/usr/bin/env python3
 
 def main():
 
     # pause the program and wait for the user to provide their name
     user_name = input("Hey there! What is your name? ")
-    
-    # display the input back to the user.
-    print(f"I hope you're doing well {user_name}, wherever you are!")
+
+    # capitalize the user's name
+    user_name = str.capitalize(user_name)
+
+    # define function that checks if the user's name contains number
+    def name_has_number(name):
+        return any(char.isdigit() for char in name)
+
+    # if the name contains number, print out a funny greeting message. Otherwise, print regular greeting message
+    if(name_has_number(user_name)):
+        print(f"Nice name you got {user_name}! Did Elon Musk name you?")
+    else:
+        print(f"I hope you're doing well {user_name}, wherever you are!")
 
     # pause the program and wait for the user to provide the day of the week
     user_day = input("What day of the week is it there? ")
+
+    # capitalize the day of the week
     user_day = str.capitalize(user_day)
 
-    print("Hello, " + user_name + "! Happy " + user_day + "!")
+    # finally, print out a greeting message with user name and the day of the week
+    print(f"Hello, {user_name}! Happy {user_day}!")
 
 main()
